d01cd9e50d20f2be462e8b95abab14330883ca25
Tori Gushulak <tori.gushulak@gmail.com>
2022-12-22 21:20:19 -0700

    todo list done for now

--- script.js
+++ script.js
@@ -1,192 +1,199 @@
-// element to hold all items
-// const container = document.getElementById("container");
-
-// creates project when title is submitted
-// let newProject = () => {
-//     // create div for project and list items
-//     const projectDiv = document.createElement('div');
-//     projectDiv.className = "project";
-
-//     // get project title input
-//     const pInput = document.getElementById("projecttitle").value;
-//     let projectTitle = document.createTextNode(pInput);
-//     // put title of project in div
-//     projectDiv.appendChild(projectTitle);
-
-//     // create an unordered list for list items
-//     const projectUl = document.createElement("ul");
-
-//     // create list 
-//     const list = (() => {
-//         const li = document.createElement("li");
-//         // create input for list item to add
-//         let getListItem = document.createElement("input");
-//         getListItem.id = "getListItem";
-//         getListItem.placeholder = "Add a task to your list";
-//         projectDiv.appendChild(getListItem);
-//         // button to submit list item
-//         const liButton = document.createElement("button");
-//         liButton.textContent = "Add to list";
-//         projectDiv.appendChild(liButton);
-//         liButton.addEventListener("click", () => {
-//             let liInput = getListItem.value;
-//             let listItem = document.createTextNode(liInput);
-//             projectUl.appendChild(listItem);
-//             // clear input after adding to list
-//             getListItem.value = '';
-//         });
-//     })();
-
-
-//     projectDiv.appendChild(projectUl);
-//     container.appendChild(projectDiv);
-// }
-
-// NEW CODE//
 let projects = [];
 
 
-class project {
+class Project {
     constructor(title, description, dueDate, priority, notes) {
         this.title = title,
             this.description = description,
             this.dueDate = dueDate,
             this.priority = priority,
-            this.notes = notes
+            this.notes = notes,
+            this.complete = false,
+            this.tasks = []
+    }
+}
+
+class Task {
+    constructor(title) {
+        this.title = title,
+            this.dueDate = new Date(),
+            this.complete = false
     }
 }
 
 const render = () => {
-    projectsDiv.textContent = "";
+    $projectsDiv.textContent = "";
 
 
     projects.forEach(project => {
-        projectCard = document.createElement('div');
-        projectCard.className = "projectcard";
-
-        projectTitle = document.createElement('h2');
-        projectTitle.textContent = project.title;
-        projectCard.appendChild(projectTitle);
-
-        projectDescription = document.createElement('p');
-        projectDescription.textContent = `Description: ${project.description}`;
-        projectCard.appendChild(projectDescription);
-
-        projectDue = document.createElement('h4');
-        projectDue.textContent = `Due: ${project.dueDate}`;
-        projectCard.appendChild(projectDue);
-
-        projectPriority = document.createElement('p');
-        projectPriority.textContent = `Importance: ${project.priority}`;
-        projectCard.appendChild(projectPriority);
-
-        projectNotes = document.createElement('p');
-        projectNotes.textContent = `Notes: ${project.notes}`;
-        projectCard.appendChild(projectNotes);
 
 
+        if (project.complete == true) {
+            // append to done div
+            // const $completedFolder = document.createElement('div');
+            // $completedFolder.classList = "completedfolder";
+            // $completedFolder.textContent = "Completed Projects";
+            // $completedFolder.appendChild(project);
+                
+        } else {
+            // append to not done div
 
-        // all todo elements go into this div
-        taskDiv = document.createElement('div');
-        taskDiv.id = "taskdiv";
-        projectCard.appendChild(taskDiv);
+        }
+
+        const $projectCard = document.createElement('div');
+        $projectCard.className = "projectcard";
+
+        const $titleCheckDiv = document.createElement('div');
+        $titleCheckDiv.id = "titlecheckdiv";
+
+        // check to complete task
+        const $newCheck = document.createElement('input');
+        $newCheck.type = "checkbox";
+        $newCheck.classList = "check";
+        $newCheck.checked = project.complete;
+        $titleCheckDiv.appendChild($newCheck)
+
+        $newCheck.addEventListener('change', () => {
+            project.complete = !project.complete;
+        })
+
+
+        // const $completedFolder = document.createElement('div');
+        // $completedFolder.classList = "completedfolder";
+        // $completedFolder.textContent = "Completed Projects";
+
+        // const index = projects.indexOf(project);
+        // if (index > -1) {
+        //     let completed = projects.splice(index, 1);
+        //     $completedFolder.appendChild(completed);
+        // }
+
+        // $projectsDiv.appendChild($completedFolder);
+        // 
+
+        const $projectTitle = document.createElement('h2');
+        $projectTitle.textContent = project.title;
+        $titleCheckDiv.appendChild($projectTitle)
+        $projectCard.appendChild($titleCheckDiv);
+
+        const $projectDescription = document.createElement('p');
+        $projectDescription.textContent = `Description: ${project.description}`;
+        $projectCard.appendChild($projectDescription);
+
+        const $projectDue = document.createElement('h4');
+        $projectDue.textContent = `Due: ${project.dueDate}`;
+        $projectCard.appendChild($projectDue);
+
+        const $projectPriority = document.createElement('p');
+        $projectPriority.textContent = `Importance: ${project.priority}`;
+        $projectCard.appendChild($projectPriority);
+
+        const $projectNotes = document.createElement('p');
+        $projectNotes.textContent = `Notes: ${project.notes}`;
+        $projectCard.appendChild($projectNotes);
+
+        // Render all todo's here
+        const $taskDiv = document.createElement('div');
+        $taskDiv.id = "taskdiv";
+        $projectCard.appendChild($taskDiv);
+
 
         // input and button go in this div
-        inputDiv = document.createElement('div');
-        inputDiv.id = "inputdiv";
+        let $inputDiv = document.createElement('div');
+        $inputDiv.id = "inputdiv";
 
         // create input to add task title
-        inputTask = document.createElement("input");
-        inputTask.id = "getListItem";
-        inputTask.placeholder = "Add a task to your list";
-        inputDiv.appendChild(inputTask);
+        let $inputTask = document.createElement("input");
+        $inputTask.id = "getListItem";
+        $inputTask.placeholder = "Add a task to your list";
+        $inputDiv.appendChild($inputTask);
 
 
         // button to submit task
-        inputButton = document.createElement("button");
-        inputButton.textContent = "Add to list";
-        inputDiv.appendChild(inputButton);
-        inputButton.addEventListener("click", () => {
+        const $inputButton = document.createElement("button");
+        $inputButton.textContent = "Add to list";
+        $inputDiv.appendChild($inputButton);
+        $taskDiv.appendChild($inputDiv);
 
-            todos = [];
 
-            task = document.createElement("div");
-            task.id = "task";
-            todos.push(task);
 
-            // if input is empty, alert 
-            taskTitle = inputTask.value;
-            if (inputTask.value == "") {
-                alert("You have to type something")
+        // 
+        $inputButton.addEventListener("click", () => {
+            // DO NOT RENDER STUFF HERE (do not push/append html to the dom = render) => ONLY THE render() FUNCTION IS ALLOWED TO DO THAT
+            // Only modify logic => re-render page
+            let taskTitle = $inputTask.value;
+            console.log("Task Title", taskTitle);
+
+            if ($inputTask.value == "") {
+                alert("Empty tasks cannot be submitted.")
             } else {
-                
-                taskTitle = document.createTextNode(taskTitle);
-                task.appendChild(taskTitle);
-                taskDiv.appendChild(task);
+                const newTask = new Task(taskTitle);
+                // console.log("Task", newTask);
 
-                // clear input after adding to list
-                inputTask.value = '';
-
-                // checkbox for each item on list
-                taskCheck = document.createElement('input');
-                taskCheck.type = "checkbox";
-                taskCheck.id = "check";
-                task.appendChild(taskCheck)
-
-                // remove button for each item on list
-                removeTask = document.createElement('button');
-                removeTask.id = 'removetask'
-                removeTask.textContent = "X";
-                task.appendChild(removeTask);
-
-                
-
-                removeTask.addEventListener('click', function () {
-                    // task.remove();
-
-                    // listItem.remove();
-                    // newCheck.remove();
-                    // removeB.remove();
-
-                    const index = todos.indexOf(task);
-                    if (index > -1) {
-                        todos.splice(index, 1);
-                    }
-                    render()
-                });
+                // On update of Model => Must re-render
+                project.tasks.push(newTask);
+                render();
             }
-        });
 
-
-        taskDiv.appendChild(inputDiv);
-
-
-
-
-
-        // label on check to complete task
-        checkLabel = document.createElement('label');
-        checkLabel.textContent = "Completed:";
-        checkLabel.id = "checklabel"
-        projectCard.appendChild(checkLabel);
-
-        // check to complete task
-        newCheck = document.createElement('input');
-        newCheck.type = "checkbox";
-        newCheck.id = "check";
-        projectCard.appendChild(newCheck)
-
-        newCheck.addEventListener('change', () => {
-            // move project to completed folder
         })
 
-        // remove project
-        removeB = document.createElement('button');
-        removeB.textContent = "Remove Project";
-        removeB.id = "removeB";
-        projectCard.appendChild(removeB);
+        // ===========================================================
+        // task stuff
+        // ===========================================================
 
-        removeB.addEventListener('click', function () {
+
+        // Render Tasks
+        // Model =(representing model in view)=> Viewing
+        project.tasks.forEach(taskObj => {
+
+            console.log("render tasks", taskObj)
+
+            // 1) make parent div
+            const $taskContainer = document.createElement('div');
+            $taskContainer.classList = "taskcontainer";
+
+            // 2) make html elements for taskObj properties
+            let $taskComplete = document.createElement("input");
+            $taskComplete.type = "checkbox";
+            $taskComplete.checked = taskObj.complete;
+            $taskComplete.addEventListener('change', () => {
+                taskObj.complete = !taskObj.complete;
+            })
+
+            let $taskTitle = document.createElement("h4");
+            $taskTitle.textContent = taskObj.title;
+
+            let $taskDate = document.createElement("h6");
+            $taskDate.id = "taskdate";
+            $taskDate.textContent = ` Due: ${taskObj.dueDate}`;
+
+            let $removeTask = document.createElement("button");
+            $removeTask.textContent = "X";
+            $removeTask.addEventListener("click", () => {
+                const index = project.tasks.indexOf(taskObj);
+                if (index > -1) {
+                    project.tasks.splice(index, 1);
+                }
+                render()
+            })
+
+            // 3) append the html elements from #2 to #1
+            $taskContainer.appendChild($taskComplete);
+            $taskContainer.appendChild($taskTitle);
+            $taskContainer.appendChild($taskDate);
+            $taskContainer.appendChild($removeTask);
+            // 4) append #1 (parent div) to project
+            $projectCard.appendChild($taskContainer);
+        })
+
+
+        // remove project
+        const $removeB = document.createElement('button');
+        $removeB.textContent = "Remove Project";
+        $removeB.id = "removeB";
+        $projectCard.appendChild($removeB);
+
+        $removeB.addEventListener('click', function () {
             const index = projects.indexOf(project);
             if (index > -1) {
                 projects.splice(index, 1);
@@ -194,28 +201,29 @@ const render = () => {
             render()
         });
 
-        projectsDiv.append(projectCard);
+        $projectsDiv.append($projectCard);
     })
+
 }
 
 
 
-let projectsDiv = document.getElementById('projectsdiv');
-let form = document.querySelector("form");
+let $projectsDiv = document.getElementById('projectsdiv');
+let $form = document.querySelector("form");
 
 // prevents submit button from reloading page when clicked
 function handleForm(event) { event.preventDefault(); }
-form.addEventListener('submit', handleForm);
+$form.addEventListener('submit', handleForm);
 
 
 form.onsubmit = function () {
-    title = document.getElementById("projecttitle").value;
-    description = document.getElementById("projectdescription").value;
-    dueDate = document.getElementById("projectdue").value;
-    priority = document.getElementById("projectpriority").value;
-    notes = document.getElementById("projectnotes").value;
+    const title = document.getElementById("projecttitle").value;
+    const description = document.getElementById("projectdescription").value;
+    const dueDate = document.getElementById("projectdue").value;
+    const priority = document.getElementById("projectpriority").value;
+    const notes = document.getElementById("projectnotes").value;
 
-    newProject = new project(title, description, dueDate, priority, notes);
+    const newProject = new Project(title, description, dueDate, priority, notes);
     projects.push(newProject);
 
     // reset dom
@@ -239,3 +247,36 @@ newProjectButton.addEventListener("click", function () {
         form.style.visibility = "hidden";
     }
 });
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (storageAvailable('localStorage')) {
+    // Yippee! We can use localStorage awesomeness
+    console.log("bean");
+  }
+  else {
+    // Too bad, no localStorage for us
+  }
